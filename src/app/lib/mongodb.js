4e7fc186d969dd87396568aa19d1fe4bb9e09221
Sagar Bhatt <sagarbhatt7451@gmail.com>
2025-05-13 09:28:44 +0530

    chnage new

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -4,20 +4,21 @@ let client;
 let clientPromise;
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const dbName = process.env.MONGODB_DB;
 
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error('Please add MONGODB_URI to your environment variables');
 }
 
+// In development, use a global variable to preserve value across module reloads caused by HMR (Hot Module Replacement)
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri); // ✅ No need for options
   global._mongoClientPromise = client.connect();
 }
 clientPromise = global._mongoClientPromise;
 
 export const connectToDatabase = async () => {
   const client = await clientPromise;
-  const db = client.db(process.env.MONGODB_DB);
+  const db = client.db(dbName || "default");
   return { client, db };
 };
