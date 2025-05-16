7ba0d913c9a06ca9f48859b1d20750e88bb81cea
Sagar Bhatt <sagarbhatt7451@gmail.com>
2025-05-16 07:12:57 +0530

    today i change something

--- src/app/_components/JobListPage.js
+++ src/app/_components/JobListPage.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import JobCard from './JobCard';  
-
-async function getJobs() {
-  const res = await fetch('http://localhost:3000/api/jobs', { 
-    cache: 'no-store' 
-  });
-  if (!res.ok) throw new Error('Failed to fetch jobs');
-  return res.json();
-}
-
-export default async function JobsPage() {
-  const jobs = await getJobs();
-
-  return (
-    <div className="container mt-4">
-      <h1 className="mb-4">Available Jobs</h1>
-      {jobs.map(job => (
-        <JobCard key={job._id} job={job} />
-      ))}
-    </div>
-  );
-=======
 import { useCallback, useState, useEffect } from "react";
 
 export default function JobSearch() {
@@ -318,5 +294,4 @@ export default function JobSearch() {
       </div>
     </div>
   );
->>>>>>> 461442d87c92a571eb501d75b69fdca6686a6732
 }
