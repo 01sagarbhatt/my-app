517d789a7d0129d43b4428fd0813383e0cdb7a91
Sagar Bhatt <sagarbhatt7451@gmail.com>
2025-06-29 22:38:25 +0530

    29/06/2025 changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,8 +21,8 @@ import Footer from './_components/Footer';
         />
       </head>
         <body className="h-100vh flex flex-col">
-          <h1>Your website has been taken down because the payment has not been made. Please make the payment first.
-</h1>
+          {/* <h1>Your website has been taken down because the payment has not been made. Please make the payment first.
+</h1> */}
            {/* <SessionWrapper>
           <Navbar />
           <main>{children}</main>
